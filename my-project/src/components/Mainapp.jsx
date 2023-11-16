384eccd2cdf0524cc123781d6522536eadfae6ab
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-16 10:42:32 +0300

    Update Mainapp.jsx

--- my-project/src/components/Mainapp.jsx
+++ my-project/src/components/Mainapp.jsx
@@ -1,14 +1,16 @@
 import Header from "./Header";
 
-const Mainapp = () => {
+const MainApp = () => {
   return (
     <>
-      <div className="app bg-theme-bg max-w-1250 max-h-860 h-[90vh] font-[500] text-[15px]
-      flex flex-col overflow-hidden relative w-full rounded-[14px] backdrop-blur-[20px]">
+      <div
+        className="app bg-theme-bg max-w-1250 max-h-860 h-[90vh] font-[500] text-[15px]
+      flex flex-col overflow-hidden relative w-full rounded-[14px] backdrop-blur-[20px]"
+      >
         <Header />
       </div>
     </>
   );
 };
 
-export default Mainapp;
+export default MainApp;
