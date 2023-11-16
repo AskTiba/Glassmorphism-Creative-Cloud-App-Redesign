8f8c14fbb1be056fe8743c4855470587432ed856
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-16 10:24:49 +0300

    Update SVG.jsx

--- my-project/src/components/SVG.jsx
+++ my-project/src/components/SVG.jsx
@@ -6,13 +6,13 @@ const Svg = () => {
       bg-[--dropdown-bg] cursor-pointer z-[3] rounded-full p-[8px]"
       >
         <svg
-          className="w-[24px] "
+          className="w-[24px] flex-shrink-0 fill-current text-yellow-400  stroke-current transition-all duration-500"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
         </svg>
