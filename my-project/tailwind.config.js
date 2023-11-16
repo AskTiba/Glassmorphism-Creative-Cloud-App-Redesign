3a2d0f7b267bf1fdf7a82b3fa38a0f67dad692fe
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-16 11:12:23 +0300

    Update tailwind.config.js

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       maxWidth: {
-        '1250': '1250px',
+        1250: "1250px",
       },
       maxHeight: {
-        '860': '860px',
+        860: "860px",
       },
       colors: {
-        'theme-bg': 'var(--theme-bg-color)', // Define your custom background color here
+        "theme-bg": "var(--theme-bg-color)", // Define your custom background color here
       },
       boxShadow: {
-        'custom': '-1px 3px 8px -1px rgba(0, 0, 0, 0.2)',
+        custom: "-1px 3px 8px -1px rgba(0, 0, 0, 0.2)",
+        custom1: "24px 0 0 0 #f8ce52",
+        custom2: "48px 0 0 0 #5fcf65",
       },
     },
   },
   plugins: [],
-}
+};
