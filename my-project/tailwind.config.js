8de9c4e7e646c8f7fa780719e4e1b7fb1ff32b2f
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-16 10:24:33 +0300

    Update tailwind.config.js

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        '1250': '1250px',
+      },
+      maxHeight: {
+        '860': '860px',
+      },
+      colors: {
+        'theme-bg': 'var(--theme-bg-color)', // Define your custom background color here
+      },
       boxShadow: {
         'custom': '-1px 3px 8px -1px rgba(0, 0, 0, 0.2)',
       },
